c671d40c0a5987ef754c8db5b2174752aa683342
suyamshah123 <siyamonfire012@gmail.com>
2025-08-21 18:51:13 +0600

    Create Offbot.js

---
+++ scripts/cmds/Offbot.js
@@ -0,0 +1,20 @@
+module.exports = {
+  config: {
+    name: "offbot",
+    version: "1.0",
+    author: "siyam",
+    countDown: 45,
+    role: 0,
+    shortDescription: "Turn off bot",
+    longDescription: "Turn off bot",
+    category: "owner",
+    guide: "{p}{n}"
+  },
+  onStart: async function ({event, api}) {
+    const permission = [ "100074722578899" ];
+  if (!permission.includes(event.senderID)) {
+    api.sendMessage("╔════ஜ۩۞۩ஜ═══╗\nমাংগেরনাটি তুই এই কমান্ড ইউজ করতে পারবি নাহ .\n═══ஜ۩۞۩ஜ═══╝", event.threadID, event.messageID);
+    return;
+  }
+    api.sendMessage("╔════ஜ۩۞۩ஜ═══╗\nবাই সুনা 💋 উম্মাহ  ✅\═══ஜ۩۞۩ஜ═══╝",event.threadID, () =>process.exit(0))}
+};
